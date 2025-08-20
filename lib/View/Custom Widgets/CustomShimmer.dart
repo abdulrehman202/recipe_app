d0a1eb5b88d15620ead4e951bb63130b96586d8d
abdulrehman202 <abdulrehman1411@gmail.com>
2025-08-20 18:52:35 +0500

    .

--- lib/View/Custom Widgets/CustomShimmer.dart
+++ lib/View/Custom Widgets/CustomShimmer.dart
@@ -5,12 +5,12 @@ import 'package:shimmer_animation/shimmer_animation.dart';
 Widget userInfoShimmer(Widget content)
 {
   return Shimmer(
-    duration: Duration(milliseconds: 1500), //Default value
-    interval: Duration(milliseconds: 5), //Default value: Duration(seconds: 0)
+    duration: const Duration(milliseconds: 1500), //Default value
+    interval: const Duration(milliseconds: 5), //Default value: Duration(seconds: 0)
     color: Constants.GREY_LABEL_COLOR, //Default value
     colorOpacity: 0.5, //Default value
     enabled: true, //Default value
-    direction: ShimmerDirection.fromLTRB(),  //Default Value
+    direction: const ShimmerDirection.fromLTRB(),  //Default Value
     child: content,
   );
 }
--- lib/View/recipe_view_screen.dart
+++ lib/View/recipe_view_screen.dart
@@ -18,16 +18,16 @@ class RecipeViewScreen extends StatelessWidget {
   late AsyncMemoizer _memoizer;
   Recipe recipe;
   bool isSAved;
-  RecipeViewScreen({super.key, required this.recipe, this.isSAved=false});
+  RecipeViewScreen({super.key, required this.recipe, this.isSAved = false});
 
   @override
   Widget build(BuildContext context) {
     _memoizer = AsyncMemoizer();
     return ChangeNotifierProvider(
-      create:  (context) => RecipeViewProvider(),
+      create: (context) => RecipeViewProvider(),
       child: Consumer<RecipeViewProvider>(
-          builder: (context, recipeView, _)=> Scaffold(
-            key: _scaffoldKey,
+        builder: (context, recipeView, _) => Scaffold(
+          key: _scaffoldKey,
           backgroundColor: Colors.white,
           appBar: _appBar(recipeView),
           body: _body(context, recipeView),
@@ -38,54 +38,60 @@ class RecipeViewScreen extends StatelessWidget {
 
   Widget _body(BuildContext context, RecipeViewProvider provider) {
     return Container(
-        margin: const EdgeInsets.symmetric(horizontal:  10.0),
-        child:
-            CustomScrollView(controller: provider.scrollController, slivers: <Widget>[
-          SliverToBoxAdapter(
-            child: Column(
-              children: [
-                SavedecipeCard(showTitle: false,recipe: recipe,),
-                _titleRow(context),
-                _userRow(context, provider),
-              ],
-            ),
-          ),
-          SliverAppBar(
-            backgroundColor: Colors.white,
-            leading: Container(),
-            pinned: true,
-            title: _tabsRow(context, provider),
-          ),
-          SliverAppBar(
-              backgroundColor: Colors.white,
-              leading: Container(),
-              pinned: true,
-              flexibleSpace: Column(children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        margin: const EdgeInsets.symmetric(horizontal: 10.0),
+        child: CustomScrollView(
+            controller: provider.scrollController,
+            slivers: <Widget>[
+              SliverToBoxAdapter(
+                child: Column(
                   children: [
-                    const Row(crossAxisAlignment: CrossAxisAlignment.end, children: [
-                      Icon(Icons.room_service_outlined),
-                      Text('1 Serve'),
-                    ]),
-                    _tabsContent(provider)
+                    SavedecipeCard(
+                      showTitle: false,
+                      recipe: recipe,
+                    ),
+                    _titleRow(context),
+                    _userRow(context, provider),
                   ],
                 ),
-              ])),
-          _tabsList(provider),
+              ),
+              SliverAppBar(
+                backgroundColor: Colors.white,
+                leading: Container(),
+                pinned: true,
+                title: _tabsRow(context, provider),
+              ),
+              SliverAppBar(
+                  backgroundColor: Colors.white,
+                  leading: Container(),
+                  pinned: true,
+                  flexibleSpace: Column(children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Row(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Icon(Icons.room_service_outlined),
+                              Text('1 Serve'),
+                            ]),
+                        _tabsContent(provider)
+                      ],
+                    ),
+                  ])),
+              _tabsList(provider),
 
-          // _tabsContent(),
+              // _tabsContent(),
 
-          // Column(
-          //   children: [
-          //     SavedecipeCard(showTitle: false),
-          //     _titleRow(context),
-          //     _userRow(context),
-          //     _tabsRow(),
-          //     _tabsContent(),
-          //   ],
-          // ),
-        ]));
+              // Column(
+              //   children: [
+              //     SavedecipeCard(showTitle: false),
+              //     _titleRow(context),
+              //     _userRow(context),
+              //     _tabsRow(),
+              //     _tabsContent(),
+              //   ],
+              // ),
+            ]));
   }
 
   PreferredSizeWidget _appBar(RecipeViewProvider provider) {
@@ -104,10 +110,10 @@ class RecipeViewScreen extends StatelessWidget {
               PopupMenuItem<String>(
                   value: 'Review',
                   child: _popUpMenuTile('Review', Icons.rate_review)),
-               PopupMenuItem<String>(
-                  value: isSAved?'Unsave':'Save',
-                  child:
-                      _popUpMenuTile(isSAved?'Unsave':'Save', Icons.bookmark_border_outlined)),
+              PopupMenuItem<String>(
+                  value: isSAved ? 'Unsave' : 'Save',
+                  child: _popUpMenuTile(isSAved ? 'Unsave' : 'Save',
+                      Icons.bookmark_border_outlined)),
             ],
           ),
         )
@@ -155,19 +161,22 @@ class RecipeViewScreen extends StatelessWidget {
 
   void _rateRecipe() {}
 
-  void _review() 
-  {
-    Navigator.push( _scaffoldKey.currentState!.context , MaterialPageRoute(builder: (ctx)=> ReviewScreen(recipeId: recipe.id,)) );
+  void _review() {
+    Navigator.push(
+        _scaffoldKey.currentState!.context,
+        MaterialPageRoute(
+            builder: (ctx) => ReviewScreen(
+                  recipeId: recipe.id,
+                )));
   }
 
-  Future<void> _unsave(RecipeViewProvider provider)
-  async{
+  Future<void> _unsave(RecipeViewProvider provider) async {
     provider.removeFromSavedRecipeList(recipe.id);
     await provider.updateSavedrecipes();
     isSAved = false;
   }
-  Future<void> _save(RecipeViewProvider provider) async
-  {
+
+  Future<void> _save(RecipeViewProvider provider) async {
     provider.addToSavedRecipeList(recipe.id);
     await provider.updateSavedrecipes();
     isSAved = true;
@@ -190,15 +199,16 @@ class RecipeViewScreen extends StatelessWidget {
                 )),
             Expanded(
                 child: Align(
-                    alignment: Alignment.centerRight, child: Text('${recipe.totalUsersWhoRated??0} rating(s)')))
+                    alignment: Alignment.centerRight,
+                    child: Text('${recipe.totalUsersWhoRated ?? 0} rating(s)')))
           ],
         ),
         Row(
           children: [
             const Icon(Icons.alarm_rounded),
             Container(
-              margin: const EdgeInsets.only(left: 5),
-              child: Text('${recipe.duration} mins')),
+                margin: const EdgeInsets.only(left: 5),
+                child: Text('${recipe.duration} mins')),
           ],
         )
       ],
@@ -206,97 +216,99 @@ class RecipeViewScreen extends StatelessWidget {
   }
 
   Widget _userRow(BuildContext context, RecipeViewProvider provider) {
-    return FutureBuilder(
-      future: _memoizer.runOnce(() async {
-          await provider.fetchUser(recipe.chefId);
-        }),
-      builder: (context, asyncSnapshot) {
-        if(asyncSnapshot.connectionState == ConnectionState.waiting)
-        {
-          return Container(
+    return FutureBuilder(future: _memoizer.runOnce(() async {
+      await provider.fetchUser(recipe.chefId);
+    }), builder: (context, asyncSnapshot) {
+      if (asyncSnapshot.connectionState == ConnectionState.waiting) {
+        return Container(
           margin: const EdgeInsets.symmetric(vertical: 30),
-            height: 50,
-            alignment: Alignment.centerLeft,
-            child: userInfoShimmer(
-                                
-                    Container(
-                      color: Colors.white,
-                    ),
-                  
-                              ),)
+          height: 50,
+          alignment: Alignment.centerLeft,
+          child: userInfoShimmer(
+            Container(
+              color: Colors.white,
+            ),
+          ),
+        )
             // child: const Text('Fetching recipe owner\'s detail...',))
             ;
-        }
-        else if(asyncSnapshot.connectionState == ConnectionState.done) {
-        if(provider.success)
-        {
+      } else if (asyncSnapshot.connectionState == ConnectionState.done) {
+        if (provider.success) {
           return Container(
-          margin: const EdgeInsets.symmetric(vertical: 30),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Expanded(
-                flex: 8,
-                child: Row(
-                  children: [
-                    ClipOval(
-                      child: SizedBox(
-                        width: 50,
-                        height: 50,
-                        child: Image.asset(
-                          Constants.BASE_IMG_PATH + Constants.DP_IMAGE,
-                          fit: BoxFit.fill,
+            margin: const EdgeInsets.symmetric(vertical: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  flex: 8,
+                  child: Row(
+                    children: [
+                      ClipOval(
+                        child: SizedBox(
+                          width: 50,
+                          height: 50,
+                          child: Image.asset(
+                            Constants.BASE_IMG_PATH + Constants.DP_IMAGE,
+                            fit: BoxFit.fill,
+                          ),
                         ),
                       ),
-                    ),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            width: MediaQuery.of(context).size.width * 0.6,
-                            margin: const EdgeInsets.only(left: 24),
-                            child: Text(
-                              provider.user!.name,
-                              style: Theme.of(context).textTheme.labelMedium,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ), 
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.location_on_sharp,
-                                color: Constants.BUTTON_COLOR,
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: MediaQuery.of(context).size.width * 0.6,
+                              margin: const EdgeInsets.only(left: 24),
+                              child: Text(
+                                provider.user!.name,
+                                style: Theme.of(context).textTheme.labelMedium,
+                                overflow: TextOverflow.ellipsis,
                               ),
-                              const Text('Faisalabad, Pakistan')
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              provider.myId == recipe.chefId?Container(): Expanded(
-                flex: 2,
-                  child:
-                      GestureDetector(onTap: () {}, child: Container(
-                        
-                        width: MediaQuery.of(context).size.width*0.1,
-                        alignment: Alignment.center,
-                        padding: const EdgeInsets.all(8.0),
-                        decoration: BoxDecoration(
-                          borderRadius: const BorderRadius.all(Radius.circular(10.0)),
-                          color: Constants.BUTTON_COLOR
+                            ),
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.location_on_sharp,
+                                  color: Constants.BUTTON_COLOR,
+                                ),
+                                const Text('Faisalabad, Pakistan')
+                              ],
+                            )
+                          ],
                         ),
-                        child: const Text('Follow',style: TextStyle(color: Colors.white), overflow: TextOverflow.fade, ))))
-            ],
-          ),
-        );}}
-        else{ Container();}
-        return Container();
+                      ),
+                    ],
+                  ),
+                ),
+                provider.myId == recipe.chefId
+                    ? Container()
+                    : Expanded(
+                        flex: 2,
+                        child: GestureDetector(
+                            onTap: () {},
+                            child: Container(
+                                width: MediaQuery.of(context).size.width * 0.1,
+                                alignment: Alignment.center,
+                                padding: const EdgeInsets.all(8.0),
+                                decoration: BoxDecoration(
+                                    borderRadius: const BorderRadius.all(
+                                        Radius.circular(10.0)),
+                                    color: Constants.BUTTON_COLOR),
+                                child: const Text(
+                                   'Follow',
+                                  style: TextStyle(color: Colors.white),
+                                  overflow: TextOverflow.fade,
+                                ))))
+              ],
+            ),
+          );
+        }
+      } else {
+        Container();
       }
-    );
+      return Container();
+    });
   }
 
   Widget _tabsRow(BuildContext context, RecipeViewProvider provider) {
@@ -315,7 +327,8 @@ class RecipeViewScreen extends StatelessWidget {
       choiceStyle: C2ChipStyle.filled(
         color: Colors.white,
         selectedStyle: C2ChipStyle(
-          padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width*0.1),
+          padding: EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.width * 0.1),
           backgroundColor: Constants.BUTTON_COLOR,
           borderRadius: const BorderRadius.all(
             Radius.circular(10),
@@ -326,15 +339,24 @@ class RecipeViewScreen extends StatelessWidget {
   }
 
   Widget _tabsContent(RecipeViewProvider provider) {
-    return Text(provider.selectedPage == 0 ? '${recipe.ingredients.length} items' : '${recipe.procedure.length} steps');
+    return Text(provider.selectedPage == 0
+        ? '${recipe.ingredients.length} items'
+        : '${recipe.procedure.length} steps');
   }
 
   Widget _tabsList(RecipeViewProvider provider) {
     return SliverList.builder(
-            itemBuilder: (BuildContext context, int index) {
-              return provider.selectedPage == 0? IngredientCard(ingredient: recipe.ingredients[index],):ProcedureCard(procedure: recipe.procedure[index],);
-            },
-            itemCount: provider.selectedPage == 0?recipe.ingredients.length:recipe.procedure.length
-          );
+        itemBuilder: (BuildContext context, int index) {
+          return provider.selectedPage == 0
+              ? IngredientCard(
+                  ingredient: recipe.ingredients[index],
+                )
+              : ProcedureCard(
+                  procedure: recipe.procedure[index],
+                );
+        },
+        itemCount: provider.selectedPage == 0
+            ? recipe.ingredients.length
+            : recipe.procedure.length);
   }
 }
