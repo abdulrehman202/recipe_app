41f36c71781f468e2264e4d8a10518491388d9cf
abdulrehman202 <abdulrehman1411@gmail.com>
2025-08-23 15:14:40 +0500

    fcm access token for web app

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -1,18 +1,19 @@
 importScripts('https://www.gstatic.com/firebasejs/9.10.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.10.0/firebase-messaging-compat.js');
+var initializeApp = require('firebase/app');
+
+// initializeApp = require('firebase/app');
 
 const firebaseConfig = {
   apiKey:  "AIzaSyCIKqC_OsFBcIpLuK-78tyUubBCAjYcl4Q",
   authDomain: "recipe-app-19207.firebaseapp.com",
   projectId: "recipe-app-19207",
   storageBucket: "recipe-app-19207.appspot.com",
-  messagingSenderId: "963673480986",
+  messagingSenderId: "1042125956309",
   appId: "1:1042125956309:web:7f31f4fe409e44887e2e5c"
 };
 
-var firebase = FirebaseWeb.instance.app;
-
-firebase.initializeApp(firebaseConfig);
+var firebase = initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
 
